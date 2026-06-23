49fdf6023b97cc32ff264723ff3afce86388de11
Naman <namanmali808@gmail.com>
2026-06-23 11:18:50 +0530

    v1.1.0: WebView onResume after onPause + visibility watchdog for background playback

--- android/app/src/main/java/com/moodflow/app/MainActivity.java
+++ android/app/src/main/java/com/moodflow/app/MainActivity.java
@@ -1,12 +1,28 @@
 package com.moodflow.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(AppUpdatePlugin.class);
         registerPlugin(MediaControlsPlugin.class);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Keep WebView alive for background YouTube playback
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().postDelayed(() -> {
+                try {
+                    getBridge().getWebView().onResume();
+                    getBridge().getWebView().requestFocus();
+                } catch (Exception ignored) {}
+            }, 300);
+        }
     }
 }
