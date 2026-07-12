63fb37eb16ae6488b3bfcc5ace8901b105918570
Naman <namanmali808@gmail.com>
2026-07-12 19:24:25 +0530

    v1.9.0: Full frontend rewrite - buttons, swipe, native-first, battery settings

--- android/app/src/main/java/com/moodflow/app/MainActivity.java
+++ android/app/src/main/java/com/moodflow/app/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.PowerManager;
 import android.provider.Settings;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -98,9 +99,10 @@ public class MainActivity extends BridgeActivity {
         try {
             PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
             if (pm != null && !pm.isIgnoringBatteryOptimizations(getPackageName())) {
-                Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                intent.setData(Uri.parse("package:" + getPackageName()));
+                Intent intent = new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+                Toast.makeText(this, "Please find MoodFlow and set 'Don't optimize' for background playback", Toast.LENGTH_LONG).show();
             }
         } catch (Exception ignored) {}
     }
